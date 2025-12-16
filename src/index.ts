51a51977b0ec165e67381dcaa476073d147ab73a
02Uriel10 <marco.zambrano6379@alumnos.udg.mx>
2025-12-16 13:21:20 -0600

    Tipado estatico

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 let text: string = "Hello";  //inferir explicito
 let text2 = "TypeScript";   // inferir no explicito
+text="1";
 function printCoord(pt:{ x: number; y: number}) {
     console.log("The coordinate's x value is " + pt.x);
     console.log("The coordinate's y value is " + pt.y);
