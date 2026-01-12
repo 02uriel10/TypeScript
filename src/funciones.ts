a9e74ee9fdd17d47f5752dd74a6505f5135d3832
02Uriel10 <marco.zambrano6379@alumnos.udg.mx>
2026-01-12 12:01:45 -0600

    Callbacks

--- src/funciones.ts
+++ src/funciones.ts
@@ -2,3 +2,12 @@ const suma: (a: number, b: number) => number = (num1: number, num2: number): num
     return num1 + num2;
 }
 console.log("Suma: " + suma(5, 10)); // Suma: 15
+//Callbacks
+const myCallback = function (text: string) {
+    console.log("Callback ejecutado: " + text);
+}
+function functCallback(text:string, callback: (text:string) => void) {
+    console.log("Funcion con callback: " + text);
+    callback(text);
+}
+functCallback("Hello from callback", myCallback);
