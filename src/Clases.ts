e8a041b8c2eb006730c28dec385f6c912fa728ce
02Uriel10 <marco.zambrano6379@alumnos.udg.mx>
2026-01-20 15:12:13 -0600

    Getters y setters

--- src/Clases.ts
+++ src/Clases.ts
@@ -1,12 +1,29 @@
-interface IPrint{
+interface Print{
     print: ()=> void;
 }
 
-class Persona{
-    id:number | undefined;
-    print():void{
-        console.log(`Imprimiendo......` + this.id);
-}
+class Persona implements Print{
+    readonly suscribe: boolean
+     constructor (private _id: number){
+        this.suscribe= true
+     }
+     private privPrint():void{
+        console.log("imprimiendo....")
+     }
+
+     get id(): number{
+        console.log("Get being used")
+        return this._id
+     }
+
+     print(): void{
+         this.privPrint()
+     }
+     set id(id:number){
+        console.log("Set being used")
+        this._id=id
+     }
+    
 }
 class Gerente{
     id:number | undefined;
@@ -14,10 +31,12 @@ class Gerente{
         console.log(`Gerente imprimiendo......` );
 }
 }
-const persona= new Persona();
-persona.id=123;
+const persona= new Persona(10);
+
+console.log(persona.suscribe)
 persona.print();
-function imprimir(obj: IPrint){
+persona.id=20
+function imprimir(obj: Print){
     obj.print();
 }
 const gerente= new Gerente()
